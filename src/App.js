4e3abb97a9fbd94e261cfee01fe982efbd9afbb9
Leocario <nicolas.constantin@students.hevs.ch>
2021-05-25 13:56:34 +0200

    add OpenSeadragon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header/Header";
+import OpenSeadragonViewer from "./OpenSeadragon/OpenSeadragonViewer";
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+    const [images, setImages] = useState([]);
+    const [selectedImage, setSelectedImage] = useState();
+
+
+    useEffect(() => {
+        getImages();
+    }, []);
+
+    const getImages = async () => {
+        const response = await fetch("https://openslide-demo.s3.dualstack.us-east-1.amazonaws.com/info.json")
+        let image = await response.json();
+        setImages(image.groups);
+    };
+
+
+
+    const selectImage = (slide) => {
+        setSelectedImage(slide.slide);
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Header/>
+        {console.log(images)}
+        <div
+            className="App"
+            style={{
+                display: "flex",
+                justifyContent:'space-between'
+            }}
         >
-          Learn React
-        </a>
-      </header>
+            <div className="OSDViewer">
+                <OpenSeadragonViewer image={selectedImage}/>
+            </div>
+            <div>
+                <h2>Test Images</h2>
+                {images.map((group, index) => {
+                    return (
+                        <div
+                            style={{
+                                display:"flex",
+                                flexDirection:'column'
+                            }}
+                        >
+                            <h3 key={index}>{group.name}</h3>
+                            {group.slides.map((slide, index) => {
+                                return (
+                                    <button
+                                        key={index}
+                                        onClick={() => {
+                                            return selectImage(slide);
+                                        }}
+                                    >
+                                        {slide.name}
+                                    </button>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+            </div>
+
+        </div>
+
     </div>
   );
 }
