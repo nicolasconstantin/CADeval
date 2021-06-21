7dcf57468a4dfbf17e79401a0a7265f33154608c
Leocario <nicolas.constantin@students.hevs.ch>
2021-06-21 21:06:48 +0200

    Add images from server

--- src/Result/Result.js
+++ src/Result/Result.js
@@ -1,7 +1,5 @@
 import './Result.css';
-import React from 'react';
-import image from '../Ressources/ImageLogo.png'
-import image2 from '../Ressources/cnn.png'
+import React, {useState, useEffect} from 'react';
 
 //The header of the app with the title
 function Result(props) {
@@ -11,6 +9,7 @@ function Result(props) {
     let xai = props.xai;
     let displayButton = props.displayButton;
     let alreadyClick = props.alreadyClick;
+    let setAlreadyClick = props.setAlreadyClick;
 
     return (
         <>
@@ -20,34 +19,44 @@ function Result(props) {
                         <h3>Characteristics</h3>
                         <p className="resultP">Location on the WSI:
                             ({coordinates[0]},{coordinates[1]}-{coordinates[2]},{coordinates[3]})</p>
-                        <p className="resultP">Selected model: <span className="bold">{cnn}</span></p>
-                        <p className="resultP">Selected xai: <span className="bold">{xai}</span></p>
                     </>)
                 :
                 null}
             {alreadyClick ?
                 (
                     <>
+                        <p className="resultP">Selected model: <span className="bold">{cnn}</span></p>
+                        <p className="resultP">Selected xai: <span className="bold">{xai}</span></p>
                         <h2>Results</h2>
                         <p className="resultP">Prediction: <span className="bold">0.87%</span></p>
+                        <p className="bold">Original zone</p>
+                        <img className="originalZone" src="https://fast.hevs.ch/cadeval/1623850982.1658227/patch_800-110.png" alt="original zone"/>
                         {xai === "none" ? null
                             :
                             xai === "xai1" ?
                                 (
-                                    <>
-                                        <p className="bold resultP">Grad-CAM</p>
-                                        <img className="resultImg" src={image} alt="Grad-CAM"/>
-                                        <p className="bold resultP">LIME</p>
-                                        <img className="resultImg" src={image2} alt="LIME"/>
-                                    </>)
+                                    <div className="contentResult">
+                                        <div className="singleResult">
+                                            <p className="bold resultP">Grad-CAM</p>
+                                            <img className="resultImg" src="https://fast.hevs.ch/cadeval/1623850982.1658227/GradCAM_patch_800-110.png.png" alt="RCVs"/>
+                                        </div>
+                                        <div className="singleResult">
+                                            <p className="bold resultP">LIME</p>
+                                            <img className="resultImg" src="https://fast.hevs.ch/cadeval/1623850982.1658227/LIME_patch_800-110.png.png" alt="LIME"/>
+                                        </div>
+                                    </div>)
                                 :
                                 (
-                                    <>
-                                        <p className="bold resultP">RCVs</p>
-                                        <img className="resultImg" src={image} alt="RCVs"/>
-                                        <p className="bold resultP">Sharp-LIME</p>
-                                        <img className="resultImg" src={image2} alt="Sharp-LIME"/>
-                                    </>
+                                    <div className="contentResult">
+                                        <div className="singleResult">
+                                            <p className="bold resultP">RCVs</p>
+                                            <img className="resultImg" src="https://fast.hevs.ch/cadeval/1623850982.1658227/GradCAM_patch_800-110.png.png" alt="RCVs"/>
+                                        </div>
+                                        <div className="singleResult">
+                                            <p className="bold resultP">Sharp-LIME</p>
+                                            <img className="resultImg" src="https://fast.hevs.ch/cadeval/1623850982.1658227/LIME_patch_800-110.png.png" alt="LIME"/>
+                                        </div>
+                                    </div>
                                 )}
                     </>)
                 :
