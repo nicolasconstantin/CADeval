4e9deb0f9c71f3b1b71b005eb2f1b763b36bc6e7
Roger Schaer <roger.schaer@hevs.ch>
2021-11-18 17:33:16 +0100

    Clean up some effect dependencies & other warnings

--- src/OpenSeadragon/OpenSeadragonViewer.js
+++ src/OpenSeadragon/OpenSeadragonViewer.js
@@ -1,112 +1,117 @@
-import './OpenSeadragon.css';
+import "./OpenSeadragon.css";
 import OpenSeaDragon from "openseadragon";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import * as Annotorious from '@recogito/annotorious-openseadragon';
-import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
+import * as Annotorious from "@recogito/annotorious-openseadragon";
+import "@recogito/annotorious-openseadragon/dist/annotorious.min.css";
+
+//the configuration for the annotation plugin
+const configAnno = { allowEmpty: true, widgets: [] };
 
 //The render of OpenSeadragon
 function OpenSeadragonViewer(props) {
+  //the setState for the image displayed by OpenSeadragon, the setDisplay and the setCoordonates
+  let image = props.sentImage;
+  let setDisplayButton = props.setDisplayButton;
+  let setCoordinates = props.setCoordinates;
 
-    //the setState for the image displayed by OpenSeadragon, the setDisplay and the setCoordonates
-    let image = props.sentImage;
-    let setDisplayButton = props.setDisplayButton;
-    let setCoordinates = props.setCoordinates;
+  //The viewer for display OpenSeadragon
+  const [viewer, setViewer] = useState(null);
+  //the annotations
+  const [anno, setAnno] = useState(null);
 
-    //The viewer for display OpenSeadragon
-    const [viewer, setViewer] = useState(null);
-    //the annotations
-    const [anno, setAnno] = useState(null);
-    //the configuration for the annotation plugin
-    const configAnno = {allowEmpty: true, widgets: []}
+  //open the viewer and manage the annotation plugin
+  useEffect(() => {
+    if (image && viewer) {
+      viewer.open(image);
+      if (anno) {
+        anno.clearAnnotations();
+      } else {
+        const annotorious = Annotorious(viewer, configAnno);
+        //call when a selection is created
+        annotorious.on("createSelection", function (annotation) {
+          //clear the previous selection
+          annotorious.clearAnnotations();
+          //skip the comment part
+          annotorious.saveSelected();
+          //display the button
+          setDisplayButton(true);
 
-
-    //open the viewer and manage the annotation plugin
-    useEffect(() => {
-        if (image && viewer) {
-            viewer.open(image);
-            if (anno) {
-                anno.clearAnnotations();
-            } else {
-                const annotorious = Annotorious(viewer, configAnno);
-                //call when a selection is created
-                annotorious.on('createSelection', function (annotation) {
-                    //clear the previous selection
-                    annotorious.clearAnnotations();
-                    //skip the comment part
-                    annotorious.saveSelected();
-                    //display the button
-                    setDisplayButton(true);
-
-                    //get the coordonates of the region selected and transform them into int
-                    //in the annotation, the index 2 and 3 are the size of the box.
-                    //I need coordonates of the opposite corner, so I must compute it.
-                    let coordinates = annotation.target.selector.value.substring(11).split(",");
-                    coordinates[0] = parseInt(coordinates[0], 10);
-                    coordinates[1] = parseInt(coordinates[1], 10);
-                    coordinates[2] = parseInt(coordinates[0], 10) + parseInt(coordinates[2], 10);
-                    coordinates[3] = parseInt(coordinates[1], 10) + parseInt(coordinates[3], 10);
-                    //Set the state with the coordinates
-                    setCoordinates(coordinates);
-                });
-                annotorious.on('changeSelectionTarget', function (annotation) {
-                    console.log(annotation);
-                    let coordinates = annotation.selector.value.substring(11).split(",");
-                    coordinates[0] = parseInt(coordinates[0], 10);
-                    coordinates[1] = parseInt(coordinates[1], 10);
-                    coordinates[2] = parseInt(coordinates[0], 10) + parseInt(coordinates[2], 10);
-                    coordinates[3] = parseInt(coordinates[1], 10) + parseInt(coordinates[3], 10);
-                    //Set the state with the coordinates
-                    setCoordinates(coordinates);
-                });
-                setAnno(annotorious);
-            }
-        }
-
-    }, [image, viewer]);
-
-    //Init OpenSeadragon
-    useEffect(() => {
-
-        const osd = OpenSeaDragon({
-            id: "openSeaDragon",
-            prefixUrl: "openseadragon-images/",
-            animationTime: 0.5,
-            blendTime: 0.1,
-            constrainDuringPan: true,
-            maxZoomPixelRatio: 2,
-            minZoomLevel: 1,
-            visibilityRatio: 1,
-            zoomPerScroll: 2,
+          //get the coordonates of the region selected and transform them into int
+          //in the annotation, the index 2 and 3 are the size of the box.
+          //I need coordonates of the opposite corner, so I must compute it.
+          let coordinates = annotation.target.selector.value
+            .substring(11)
+            .split(",");
+          coordinates[0] = parseInt(coordinates[0], 10);
+          coordinates[1] = parseInt(coordinates[1], 10);
+          coordinates[2] =
+            parseInt(coordinates[0], 10) + parseInt(coordinates[2], 10);
+          coordinates[3] =
+            parseInt(coordinates[1], 10) + parseInt(coordinates[3], 10);
+          //Set the state with the coordinates
+          setCoordinates(coordinates);
         });
+        annotorious.on("changeSelectionTarget", function (annotation) {
+          console.log(annotation);
+          let coordinates = annotation.selector.value.substring(11).split(",");
+          coordinates[0] = parseInt(coordinates[0], 10);
+          coordinates[1] = parseInt(coordinates[1], 10);
+          coordinates[2] =
+            parseInt(coordinates[0], 10) + parseInt(coordinates[2], 10);
+          coordinates[3] =
+            parseInt(coordinates[1], 10) + parseInt(coordinates[3], 10);
+          //Set the state with the coordinates
+          setCoordinates(coordinates);
+        });
+        setAnno(annotorious);
+      }
+    }
+  }, [image, viewer, configAnno, setCoordinates, setDisplayButton]);
 
-        setViewer(osd);
+  //Init OpenSeadragon
+  useEffect(() => {
+    const osd = OpenSeaDragon({
+      id: "openSeaDragon",
+      prefixUrl: "openseadragon-images/",
+      animationTime: 0.5,
+      blendTime: 0.1,
+      constrainDuringPan: true,
+      maxZoomPixelRatio: 2,
+      minZoomLevel: 1,
+      visibilityRatio: 1,
+      zoomPerScroll: 2,
+    });
 
-        return () => {
-            viewer && viewer.destroy();
-        };
-    }, []);
+    setViewer(osd);
 
-    //Display the viewer and the message if no image is selected
-    return (
-        <>
-            <p className="hintAnnotorious">To select an area, hold the SHIFT key while clicking and dragging the
-                mouse</p>
-            <div
-                id="openSeaDragon"
-                style={{
-                    width: "100%",
-                    height: "450px"
-                }}
-            >
+    return () => {
+      viewer && viewer.destroy();
+    };
+  }, []);
 
-            </div>
+  //Display the viewer and the message if no image is selected
+  return (
+    <>
+      <p className="hintAnnotorious">
+        To select an area, hold the SHIFT key while clicking and dragging the
+        mouse
+      </p>
+      <div
+        id="openSeaDragon"
+        style={{
+          width: "100%",
+          height: "450px",
+        }}
+      ></div>
 
-            {image === null ?
-                <p className="selectImageErrorMessage">Please select an image to display</p> : null
-            }
-        </>
-    );
+      {image === null ? (
+        <p className="selectImageErrorMessage">
+          Please select an image to display
+        </p>
+      ) : null}
+    </>
+  );
 }
 
 export default OpenSeadragonViewer;
--- src/Result/Result.js
+++ src/Result/Result.js
@@ -1,107 +1,146 @@
-import './Result.css';
-import React from 'react';
+import "./Result.css";
+import React from "react";
 
 //The header of the app with the title
 function Result(props) {
+  let coordinates = props.coordinates;
+  let cnn = props.cnn;
+  let xai = props.xai;
+  let displayButton = props.displayButton;
+  let responseReady = props.responseReady;
+  let folderPath = props.folderPath;
+  let imagePath = props.imagePath;
+  let result = props.result;
+  let sourceImage = props.sourceImage;
+  let error = props.error;
 
-    let coordinates = props.coordinates;
-    let cnn = props.cnn;
-    let xai = props.xai;
-    let displayButton = props.displayButton;
-    let responseReady = props.responseReady;
-    let folderPath = props.folderPath;
-    let imagePath = props.imagePath;
-    let result = props.result;
-    let sourceImage = props.sourceImage;
-    let error = props.error;
-
-    return (
+  return (
+    <>
+      {displayButton ? (
         <>
-            {displayButton ?
-                (
-                    <>
-                        <h3>Characteristics</h3>
-                        <p className="resultCharacteristics">Location on the WSI:
-                            ({coordinates[0]},{coordinates[1]}-{coordinates[2]},{coordinates[3]})</p>
-                        <p className="resultCharacteristics">Width : {coordinates[2]-coordinates[0]} pixels - Height : {coordinates[3]-coordinates[1]} pixels</p>
-                        <p className="resultCharacteristics">Image: {sourceImage.substring(0, sourceImage.length-4)}</p>
-                    </>)
-                :
-                null}
-            {responseReady ?
-                error === null ?
-                (
-                    <>
-                        <p className="resultCharacteristics">Selected model: <span className="bold">{cnn}</span></p>
-                        <p className="resultCharacteristics">Selected xai: <span className="bold">{xai}</span></p>
-                        <h2>Results</h2>
-                        {xai === "none" ?
-                            (
-                                <>
-                                    <div className="resultDiv">
-                                        <p className="bold resultP">Original zone</p>
-                                        <p className="bold resultP">Prediction</p>
-                                    </div>
-                                    {imagePath.map((image, index) =>
-                                        (
-                                            <div className="resultDiv" key={index}>
-                                                <img className="resultImg" src={`https://fast.hevs.ch/cadeval/${folderPath}/${image}`} alt="original zone"/>
-                                                <p className="bold resultText">{parseFloat(result[index]).toFixed(3)}</p>
-                                            </div>
-                                        )
-                                    )}
-                                </>
-                            )
-                            :
-                            xai === "xai1" ?
-                                (
-                                    <>
-                                        <div className="resultDiv">
-                                            <p className="bold resultP">Original zone</p>
-                                            <p className="bold resultP">Grad-CAM</p>
-                                            <p className="bold resultP">LIME</p>
-                                            <p className="bold resultP">Prediction</p>
-                                        </div>
-                                        {imagePath.map((image, index) =>
-                                            (
-                                                <div className="resultDiv" key={index}>
-                                                    <img className="resultImg" src={`https://fast.hevs.ch/cadeval/${folderPath}/${image}`} alt="original zone"/>
-                                                    <img className="resultImg" src={`https://fast.hevs.ch/cadeval/${folderPath}/GradCAM_${image}.png`} alt="Grad-CAM"/>
-                                                    <img className="resultImg" src={`https://fast.hevs.ch/cadeval/${folderPath}/LIME_${image}.png`} alt="LIME"/>
-                                                    <p className="bold resultText">{parseFloat(result[index]).toFixed(3)}</p>
-                                                </div>
-                                            )
-                                        )}
-                                    </>
-                                )
-                                :
-                                (
-                                    <>
-                                        <p>You can see the full size image of the RCV if you click on it</p>
-                                        <div className="resultDiv">
-                                            <p className="bold resultP">Original zone</p>
-                                            <p className="bold resultPRCV">RCV</p>
-                                            <p className="bold resultP">Sharp-LIME</p>
-                                            <p className="bold resultPPredictionRCV">Prediction</p>
-                                        </div>
-                                        {imagePath.map((image, index) =>
-                                            (
-                                                <div className="resultDiv" key={index}>
-                                                    <img className="resultImg" src={`https://fast.hevs.ch/cadeval/${folderPath}/${image}`} alt="original zone"/>
-                                                    <a className="resultImgRCV" href={`https://fast.hevs.ch/cadeval/${folderPath}/RCV_${image}.png`} target="_blank"><img className="test" src={`https://fast.hevs.ch/cadeval/${folderPath}/RCV_${image}.png`} alt="RCV"/></a>
-                                                    <img className="resultImg" src={`https://fast.hevs.ch/cadeval/${folderPath}/Sharp-LIME_${image}.png`} alt="Sharp-LIME"/>
-                                                    <p className="bold resultText">{parseFloat(result[index]).toFixed(3)}</p>
-                                                </div>
-                                            )
-                                        )}
-                                    </>
-                                )}
-                    </>)
-                :
-                    <p className="ErrorMessage">{error}</p>
-            : null}
+          <h3>Characteristics</h3>
+          <p className="resultCharacteristics">
+            Location on the WSI: ({coordinates[0]},{coordinates[1]}-
+            {coordinates[2]},{coordinates[3]})
+          </p>
+          <p className="resultCharacteristics">
+            Width : {coordinates[2] - coordinates[0]} pixels - Height :{" "}
+            {coordinates[3] - coordinates[1]} pixels
+          </p>
+          <p className="resultCharacteristics">
+            Image: {sourceImage.substring(0, sourceImage.length - 4)}
+          </p>
         </>
-    );
+      ) : null}
+      {responseReady ? (
+        error === null ? (
+          <>
+            <p className="resultCharacteristics">
+              Selected model: <span className="bold">{cnn}</span>
+            </p>
+            <p className="resultCharacteristics">
+              Selected xai: <span className="bold">{xai}</span>
+            </p>
+            <h2>Results</h2>
+            {xai === "none" ? (
+              <>
+                <div className="resultDiv">
+                  <p className="bold resultP">Original zone</p>
+                  <p className="bold resultP">Prediction</p>
+                </div>
+                {imagePath.map((image, index) => (
+                  <div className="resultDiv" key={index}>
+                    <img
+                      className="resultImg"
+                      src={`https://fast.hevs.ch/cadeval/${folderPath}/${image}`}
+                      alt="original zone"
+                    />
+                    <p className="bold resultText">
+                      {parseFloat(result[index]).toFixed(3)}
+                    </p>
+                  </div>
+                ))}
+              </>
+            ) : xai === "xai1" ? (
+              <>
+                <div className="resultDiv">
+                  <p className="bold resultP">Original zone</p>
+                  <p className="bold resultP">Grad-CAM</p>
+                  <p className="bold resultP">LIME</p>
+                  <p className="bold resultP">Prediction</p>
+                </div>
+                {imagePath.map((image, index) => (
+                  <div className="resultDiv" key={index}>
+                    <img
+                      className="resultImg"
+                      src={`https://fast.hevs.ch/cadeval/${folderPath}/${image}`}
+                      alt="original zone"
+                    />
+                    <img
+                      className="resultImg"
+                      src={`https://fast.hevs.ch/cadeval/${folderPath}/GradCAM_${image}.png`}
+                      alt="Grad-CAM"
+                    />
+                    <img
+                      className="resultImg"
+                      src={`https://fast.hevs.ch/cadeval/${folderPath}/LIME_${image}.png`}
+                      alt="LIME"
+                    />
+                    <p className="bold resultText">
+                      {parseFloat(result[index]).toFixed(3)}
+                    </p>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <>
+                <p>
+                  You can see the full size image of the RCV if you click on it
+                </p>
+                <div className="resultDiv">
+                  <p className="bold resultP">Original zone</p>
+                  <p className="bold resultPRCV">RCV</p>
+                  <p className="bold resultP">Sharp-LIME</p>
+                  <p className="bold resultPPredictionRCV">Prediction</p>
+                </div>
+                {imagePath.map((image, index) => (
+                  <div className="resultDiv" key={index}>
+                    <img
+                      className="resultImg"
+                      src={`https://fast.hevs.ch/cadeval/${folderPath}/${image}`}
+                      alt="original zone"
+                    />
+                    <a
+                      className="resultImgRCV"
+                      href={`https://fast.hevs.ch/cadeval/${folderPath}/RCV_${image}.png`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <img
+                        className="test"
+                        src={`https://fast.hevs.ch/cadeval/${folderPath}/RCV_${image}.png`}
+                        alt="RCV"
+                      />
+                    </a>
+                    <img
+                      className="resultImg"
+                      src={`https://fast.hevs.ch/cadeval/${folderPath}/Sharp-LIME_${image}.png`}
+                      alt="Sharp-LIME"
+                    />
+                    <p className="bold resultText">
+                      {parseFloat(result[index]).toFixed(3)}
+                    </p>
+                  </div>
+                ))}
+              </>
+            )}
+          </>
+        ) : (
+          <p className="ErrorMessage">{error}</p>
+        )
+      ) : null}
+    </>
+  );
 }
 
 export default Result;
