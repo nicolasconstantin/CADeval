f3f633e748cafb80d1dcd11929faf85a06a18cdf
Roger Schaer <roger.schaer@hevs.ch>
2021-11-18 17:23:39 +0100

    Make folders of images dynamic

--- src/GetImages/GetImages.js
+++ src/GetImages/GetImages.js
@@ -1,81 +1,66 @@
-import "./GetImages.css"
-import React, { useEffect, useState } from "react";
+import "./GetImages.css";
+import React, { useEffect, useMemo, useState } from "react";
 import ImageToDisplay from "./ImageToDisplay";
 
 //The function to read a local folder and display a list of his content
 function GetImages(props) {
+  //the setState for the image displayed by OpenSeadragon
+  const setImage = props.setImage;
+  //State for the list of images
+  const [listOfPath, setListOfPath] = useState([]);
+  //State for the folder selected
+  const [folderSelected, setFolderSelected] = useState(null);
 
-    //the setState for the image displayed by OpenSeadragon
-    const setImage = props.setImage
-    //State for the list of images
-    const [listOfPath, setListOfPath] = useState([]);
-    //State for the center selected
-    const [centerSelected, setCenterSelected] = useState(null);
+  const setSourceImage = props.setSourceimage;
 
-    const setSourceImage = props.setSourceimage;
+  const listOfFolders = useMemo(() => {
+    let folders = listOfPath.map((path) => path.substr(0, path.indexOf("/")));
 
-    //get the list of all .tif into a folder and put it into the state
-    useEffect(() => {
-        async function ReadFolder() {
+    return Array.from(new Set(folders));
+  }, [listOfPath]);
 
-            //ask response from http request (list of images on the nas)
-            let response = await fetch('https://cb95959001d2.ngrok.io');
+  //get the list of all .tif into a folder and put it into the state
+  useEffect(() => {
+    async function ReadFolder() {
+      //ask response from http request (list of images on the nas)
+      let response = await fetch("https://cb95959001d2.ngrok.io");
 
-            setListOfPath(await response.json());
-
-        }
-        ReadFolder();
-    }, []);
-
-    const changeCenterSelected = (center) => {
-        if(center === centerSelected){
-            setCenterSelected("no center selected");
-        } else{
-            setCenterSelected(center);
-        }
+      setListOfPath(await response.json());
     }
+    ReadFolder();
+  }, []);
 
-    //Display the list of all images into an html list
-    return (
+  const changeFolderSelected = (folder) => {
+    if (folder === folderSelected) {
+      setFolderSelected("no folder selected");
+    } else {
+      setFolderSelected(folder);
+    }
+  };
+
+  //Display the list of all images into an html list
+  return (
+    <>
+      {listOfFolders.map((folder) => (
         <>
-
-            <p className="CenterTitle" onClick={() => changeCenterSelected("centre_0")}>{centerSelected==="centre_0"? "▽" : "▷"} Center 0</p>
-            {centerSelected==="centre_0"?
-                    <ImageToDisplay center={centerSelected} setImage={setImage} listOfPath={listOfPath} setSourceImage={setSourceImage}/>
-                :
-                    null
-            }
-
-            <p className="CenterTitle" onClick={() => changeCenterSelected("centre_1")}>{centerSelected==="centre_1"? "▽" : "▷"} Center 1</p>
-            {centerSelected==="centre_1"?
-                <ImageToDisplay center={centerSelected} setImage={setImage} listOfPath={listOfPath} setSourceImage={setSourceImage}/>
-                :
-                null
-            }
-
-            <p className="CenterTitle" onClick={() => changeCenterSelected("centre_2")}>{centerSelected==="centre_2"? "▽" : "▷"} Center 2</p>
-            {centerSelected==="centre_2"?
-                <ImageToDisplay center={centerSelected} setImage={setImage} listOfPath={listOfPath} setSourceImage={setSourceImage}/>
-                :
-                null
-            }
-
-            <p className="CenterTitle" onClick={() => changeCenterSelected("centre_3")}>{centerSelected==="centre_3"? "▽" : "▷"} Center 3</p>
-            {centerSelected==="centre_3"?
-                <ImageToDisplay center={centerSelected} setImage={setImage} listOfPath={listOfPath} setSourceImage={setSourceImage}/>
-                :
-                null
-            }
-
-            <p className="CenterTitle" onClick={() => changeCenterSelected("centre_4")}>{centerSelected==="centre_4"? "▽" : "▷"} Center 4</p>
-            {centerSelected==="centre_4"?
-                <ImageToDisplay center={centerSelected} setImage={setImage} listOfPath={listOfPath} setSourceImage={setSourceImage}/>
-                :
-                null
-            }
-
+          <p
+            className="FolderTitle"
+            onClick={() => changeFolderSelected(folder)}
+          >
+            {folderSelected === folder ? "▽" : "▷"} {folder}
+          </p>
+          {folderSelected === folder ? (
+            <ImageToDisplay
+              folder={folderSelected}
+              setImage={setImage}
+              listOfPath={listOfPath}
+              setSourceImage={setSourceImage}
+            />
+          ) : null}
         </>
-    );
+      ))}
+    </>
+  );
 }
 
 export default GetImages;
--- src/GetImages/ImageToDisplay.js
+++ src/GetImages/ImageToDisplay.js
@@ -1,36 +1,43 @@
-import "./GetImages.css"
+import "./GetImages.css";
 import React from "react";
 
-//The function to display the patients of one center
+//The function to display the patients of one folder
 function ImageToDisplay(props) {
+  //the setState for the folder selected, the image displayed by OpenSeadragon and the list of path
+  const folder = props.folder;
+  const setImage = props.setImage;
+  const listOfPath = props.listOfPath;
+  const setSourceImage = props.setSourceImage;
 
-    //the setState for the center selected, the image displayed by OpenSeadragon and the list of path
-    const center = props.center;
-    const setImage = props.setImage;
-    const listOfPath = props.listOfPath;
-    const setSourceImage = props.setSourceImage;
-
-    //When the user click on an image
-    const setImageDisplayed = (path) => {
-        //set the state and the storage(persistency)
-        setImage(process.env.REACT_APP_IIP_URL + path + ".dzi");
-        localStorage.setItem("image", process.env.REACT_APP_IIP_URL + path + ".dzi");
-
-        setSourceImage(path);
-        localStorage.setItem("sourceImage", path);
-    }
-
-    //Display the list of all images into an html list
-    return (
-            <ul>
-                {listOfPath.map((path) =>
-                    path.substring(0,8)===center?
-                        <li key={path} className="ItemList" onClick={() => setImageDisplayed(path)}>{center==="centre_4"? path.substring(13) : path.substring(9)}</li>
-                    :
-                        null
-                )}
-            </ul>
+  //When the user click on an image
+  const setImageDisplayed = (path) => {
+    //set the state and the storage(persistency)
+    setImage(process.env.REACT_APP_IIP_URL + path + ".dzi");
+    localStorage.setItem(
+      "image",
+      process.env.REACT_APP_IIP_URL + path + ".dzi"
     );
+
+    setSourceImage(path);
+    localStorage.setItem("sourceImage", path);
+  };
+
+  //Display the list of all images into an html list
+  return (
+    <ul>
+      {listOfPath.map((path) =>
+        path.substring(0, path.indexOf("/")) === folder ? (
+          <li
+            key={path}
+            className="ItemList"
+            onClick={() => setImageDisplayed(path)}
+          >
+            {path.substr(path.indexOf("/") + 1)}
+          </li>
+        ) : null
+      )}
+    </ul>
+  );
 }
 
 export default ImageToDisplay;
